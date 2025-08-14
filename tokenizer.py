c8fe36207dd3fdf98fb98cfbb28548f2f6fa1a5e
Krishsidd8 <krishsidd8@gmail.com>
2025-08-13 19:43:02 -0700

    added t2i to LatexTokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -3,11 +3,13 @@ class LatexTokenizer:
         self.specials = ['<PAD>', '<SOS>', '<EOS>', '<UNK>']
         self.vocab = self.specials.copy()
         self.t2i = {tok: i for i, tok in enumerate(self.vocab)}
+        self.i2t = {i: tok for tok, i in self.t2i.items()}
 
     def build_vocab(self, texts):
         toks = set(tok for txt in texts for tok in txt.split())
         self.vocab = self.specials + sorted(toks)
         self.t2i = {tok: i for i, tok in enumerate(self.vocab)}
+        self.i2t = {i: tok for tok, i in self.t2i.items()}
 
     def encode(self, txt):
         return [self.t2i['<SOS>']] + [self.t2i.get(tok, self.t2i['<UNK>']) for tok in txt.split()] + [self.t2i['<EOS>']]
